62e0828b8ce206aaf615a761d27db7fc82f94705
Swikriti <swikritikumari81@gmail.com>
2026-05-06 21:35:28 +0530

    I added my If Else program

---
+++ If_Else.java
@@ -0,0 +1,16 @@
+public class If_Else 
+{
+  public static void main(String[]args)
+   {
+    int num=5;
+    if(num%2==0)
+    {
+        System.out.println("this is an even number");
+    }
+    else
+    {
+     System.out.println("this is an odd number");
+      }
+    }
+  }
+    
