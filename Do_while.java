73e1031fb27886f88619803b42dcf4828cc2e67f
Swikriti <swikritikumari81@gmail.com>
2026-05-07 10:05:14 +0530

    Do while loop in java

---
+++ Do_while.java
@@ -0,0 +1,14 @@
+public class Do_while 
+{
+    public static void main(String[] args)
+     {
+        int i=0;
+    do
+   {
+     System.out.println("java Programming");
+     i++;
+     }
+     while(i<=10);
+     }
+    }
+
