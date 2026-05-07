93d4c32e5fd236c3aec2295fe98ef8b65655fd46
Swikriti <swikritikumari81@gmail.com>
2026-05-07 10:06:52 +0530

    while loop in java

---
+++ Whileloop.java
@@ -0,0 +1,12 @@
+public class Whileloop
+ {
+    public static void main(String[] args)
+     {
+        int no=1;
+        while(no<=20)
+        {
+            System.out.println("while loop in java");
+            no=no+1;
+        }
+    }
+}
