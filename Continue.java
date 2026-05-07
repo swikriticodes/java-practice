03636a566a2806ab2c9fa688179acdb5ea7ee912
Swikriti <swikritikumari81@gmail.com>
2026-05-07 09:59:00 +0530

    Continue Statement in java

---
+++ Continue.java
@@ -0,0 +1,15 @@
+public class Continue
+ {
+    public static void main(String[] args)
+     {
+        for(int no=1;no<=10;no++)
+        {
+            if(no==4)
+            {
+                continue;
+            }
+            System.out.println(no);
+        }
+    }
+}
+ 
