4865c2af43b810531f63b1bcea9aa2570a1ee5d2
Swikriti <swikritikumari81@gmail.com>
2026-05-07 10:03:42 +0530

    For each loop in java

---
+++ For_each.java
@@ -0,0 +1,12 @@
+public class For_each 
+{
+    public static void main(String[] args)
+     {
+        int arr[]={10,20,30,40};
+        for(int b:arr)
+        {
+            System.out.println(b);
+        }
+        
+    }
+}
